115b14b83f3d6aec2c2b8d9e218a62ab996fe893
Tandin Penjor <02230227.cst@rub.edu.bt>
2024-02-26 13:21:01 +0530

    second commit

---
+++ logicial.py
@@ -0,0 +1,34 @@
+#for AND operator
+#example 1
+x = 5
+print(x > 3 and x < 10)
+
+#example 2
+y = 12
+print(y > 10 and y % 5 == 0)
+
+#for OR operator
+#example 1
+x = 5
+print(x < 3 or x > 10)
+
+#example 2
+y = 12
+print(y < 10 or y % 2 == 0)
+
+#for NOT operator
+#example 1
+x = 5
+print(not(x > 3 and x < 10))
+
+#example 2
+y = 12
+print(not(y > 10 and y % 5 == 0))
+
+#classwork
+t=10
+print(t>2 and t<3)
+
+r=0.57
+print(r == 2*2 or r >0.34)
+
