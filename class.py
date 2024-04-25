72ae2d7f814986dd989fe88386ed0291b13cb9e5
Tandin Penjor <02230227.cst@rub.edu.bt>
2024-04-25 10:30:11 +0530

    d commit

---
+++ class.py
@@ -0,0 +1,11 @@
+class student:
+    def __init__(self, name, age ) :
+        self.name = name
+        self.age = age
+
+sonam = student("sonam lhamo", 20)
+karma = student("karma dorji", 23)
+
+print(sonam.name)
+print(sonam.age)
+print(karma.age)
